5a76e962aee78652f403f4e5cb3d9cb9f5df8031
Ravinder Singh <T00725333@mytru.ca>
2025-09-10 13:00:26 -0700

    Enter key Functionality Added

--- src/App.jsx
+++ src/App.jsx
@@ -34,11 +34,14 @@ function App() {
   return (
     <div className='overflow-x-hidden h-screen'>
       <div className=' md:w-4/5 h-15 md:my-4 mx-2 m-2 rounded-md shadow-md md:mx-auto'>
-        <input className='w-3/4 h-full text-center rounded-tl-md rounded-bl-md border-2 border-black outline-none' placeholder="Enter task here " type="text" value={inputText} onChange={(e)=> setInputText(e.target.value)}/>
-        <input className='w-1/4 h-full bg-black rounded-tr-md  rounded-br-md text-white submit-btn' type="submit" onClick={(e)=>{addTodo(e)}}/>
+        <input className='w-3/4 h-full text-center rounded-tl-md rounded-bl-md border-2 border-black outline-none' placeholder="Enter task here " type="text" value={inputText} onChange={(e)=> setInputText(e.target.value)} onKeyDown={(e)=>{ if(e.key === 'Enter'){
+          addTodo(e)
+        }}}/>
+        <input className='w-1/4 h-full bg-black rounded-tr-md  rounded-br-md text-white submit-btn' type="submit" onClick={(e)=>{addTodo(e)}} 
+        />
       </div>
       
-    <div className={`border-2 m-2 rounded-md shadow-md p-2 md:w-4/5 md:mx-auto overflow-auto max-h-[70vh] ${todos.length ===0 ?'hidden' : ''} `}>
+    <div className={`border-2 m-2 rounded-md shadow-md p-2 md:w-4/5 md:mx-auto overflow-auto max-h-[85vh] ${todos.length ===0 ?'hidden' : ''} `}>
           <h1 className={` mx-auto mb-7 md:mx-12 md:my-4 text-2xl  font-bold `}>Things to be done today : </h1>
 
 
